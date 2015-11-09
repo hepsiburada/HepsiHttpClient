f754cbf3f0056a4e2de5683d0826c655075568f9
Onat Yiğit Mercan <onatmercan@gmail.com>
2015-11-09 23:07:29 +0200

    adds configurable http client factory logic to create http client

---
+++ src/Hepsi.Http.Client/ConfigurableHttpClientFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Hepsi.Http.Client
+{
+    public class ConfigurableHttpClientFactory : IHttpClientFactory
+    {
+        private readonly HttpClientBuilder builder;
+
+        public ConfigurableHttpClientFactory(HttpClientBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public HttpClient CreateHttpClient(string baseAddress)
+        {
+            var httpClient = builder.Build();
+            httpClient.BaseAddress = new Uri(baseAddress);
+
+            return httpClient;
+        }
+
+        public HttpClient CreateHttpClient()
+        {
+            return builder.Build();
+        }
+    }
+}
